64ca5fdf41aaadf603719808897ef7eddb1bb697
Carlinz73 <carloseduardojesusf@gmail.com>
2026-09-06 17:46:40 -0300

    Delete adicionar_patio_corredor.sql

--- adicionar_patio_corredor.sql
+++
@@ -1,6 +0,0 @@
--- IXHEI - adicionar Pátio e Corredor
--- Execute UMA VEZ no Supabase > SQL Editor.
--- Converte "room" para texto para aceitar 1..14, patio e corredor.
-alter table public.items
-alter column room type text
-using room::text;
